b8f204fac78d23d6b9ec990d5e0162b561dc114b
Rachael <github@rachael.io>
2020-12-01 11:47:54 -0800

    Add Detail view for Persons in Home view

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,66 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import { data } from '../../api';
+import Person from '../person';
 import './Home.scss';
 
 function Home() {
+  const findSpouse = (person) => {
+    if(person.spouseId !== undefined) return data.find((person2) => person2.id === person.spouseId);
+    return undefined;
+  }
+
+  const findParent1 = (person) => {
+    if(person.parentId1 !== undefined) return data.find((person2) => person2.id === person.parentId1);
+    return undefined;
+  }
+
+  const findParent2 = (person) => {
+    if(person.parentId2 !== undefined) return data.find((person2) => person2.id === person.parentId2);
+    return undefined;
+  }
+
+  const [openDetailViews, setOpenDetailViews] = useState({});
+  const openDetailView = (person) => {
+    setOpenDetailViews({
+      ...openDetailViews,
+      [person.id]: true,
+    });
+  }
+
+  const closeDetailView = (person) => {
+    setOpenDetailViews({
+      ...openDetailViews,
+      [person.id]: false,
+    })
+  }
+
   return (
     <div className="Home">
       <header className="Home-Header"><h1>Family Tree</h1></header>
       <div className="Home-Persons">
         {data.map(person => (
-          <div className="Home-Person">
-            <p>{person.name}</p>
-            <p>Born {person.born}</p>
-            <p>Hometown {person.hometown}</p>
-            {person.spouseId !== undefined && <p>Spouse {data.find((person2) => person2.id === person.spouseId).name}</p>}
-            {person.parentId1 !== undefined && <p>Parent {data.find((person2) => person2.id === person.parentId1).name}</p>}
-            {person.parentId2 !== undefined && <p>Parent {data.find((person2) => person2.id === person.parentId2).name}</p>}
+          <div
+            key={person.id}
+            className={classNames("Home-Person", { 'show-detail': openDetailViews[person.id] })}
+          >
+            <div className="Home-Person_default-view" onClick={() => openDetailView(person)}>
+              <p>{person.name}</p>
+              <p>Born {person.born}</p>
+              <p>Hometown {person.hometown}</p>
+              {person.spouseId !== undefined && <p>Spouse {findSpouse(person).name}</p>}
+              {person.parentId1 !== undefined && <p>Parent {findParent1(person).name}</p>}
+              {person.parentId2 !== undefined && <p>Parent {findParent2(person).name}</p>}
+            </div>
+            <div className="Home-Person_detail-view">
+              <Person
+                person={person}
+                spouse={findSpouse(person)}
+                parent1={findParent1(person)}
+                parent2={findParent2(person)}
+                hideDetail={closeDetailView}
+              />
+            </div>
           </div>
         ))}
       </div>
--- src/components/person/Person.jsx
+++ src/components/person/Person.jsx
@@ -1,8 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Person.scss';
 
-function Person() {
+function Person({
+  person,
+  spouse,
+  parent1,
+  parent2,
+  hideDetail,
+}) {
   return (
     <div className="Person">
+      <div
+        className="Detail-Hide-Button"
+        onClick={() => hideDetail(person)}
+      >
+        <FontAwesomeIcon icon={faTimes} title="Detail-Hide-Button" />
+      </div>
+      <div className="Person-Parents">
+        {parent1 && (
+          <div className="Person-Parent1">
+            <p>Parent {parent1.name}</p>
+            <p>Born {parent1.born}</p>
+            <p>Hometown {parent1.hometown}</p>
+          </div>
+        )}
+        {parent2 && (
+          <div className="Person-Parent2">
+            <p>Parent {parent2.name}</p>
+            <p>Born {parent2.born}</p>
+            <p>Hometown {parent2.hometown}</p>
+          </div>
+        )}
+      </div>
+      <div className="Person-Spouse-Row">
+        <div className="Person-Person">
+          <p>{person.name}</p>
+          <p>Born {person.born}</p>
+          <p>Hometown {person.hometown}</p>
+        </div>
+        {spouse && (
+          <div className="Person-Spouse">
+            <p>Spouse {spouse.name}</p>
+            <p>Born {spouse.born}</p>
+            <p>Hometown {spouse.hometown}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
